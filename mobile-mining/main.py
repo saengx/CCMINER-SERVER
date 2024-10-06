c57bbc3b8c7817991119d81420625fbd71e37b15
saengx <157899625+saengx@users.noreply.github.com>
2024-10-06 21:59:52 +0700

    Update main.py

--- mobile-mining/main.py
+++ mobile-mining/main.py
@@ -5,30 +5,28 @@ import pip
 from config import banner
 
 try:
-    with open("set-miner/mining-control.json", encoding="utf-8") as set:
+    with open("setip/ip.json", encoding="utf-8") as set:
             load = set.read()
             loads = json.loads(load)
-            allow = loads['allow']
-            bind = loads['bind']
-            key = loads['key']
-        
-    os.system(f"cd set-miner && wget -N --timeout 20 --connect-timeout=30 -t 2 --no-check-certificate https://raw.githubusercontent.com/saengx/miner/main/mining-control.json")
+            ip = loads['ip']
+
+    os.system(f"cd set-miner && wget -N --timeout 10 --connect-timeout=15 -t 1 https://{ip}/online.json")
     time.sleep(2)
-    from progress.bar import ShadyBar
+    from progress.bar import ChargingBar
 except ImportError:
     pip.main(['install', '--user', 'progress'])
-    from progress.bar import ShadyBar
+    from progress.bar import ChargingBar
 
 try:
     import requests
 except ImportError:
     pip.main(['install', '--user', 'requests'])
     import requests
-    
-    
+
+
 zergpool = ["stratum+tcp://verushash.mine.zergpool.com:3300","stratum+tcp://verushash.na.mine.zergpool.com:3300","stratum+tcp://verushash.eu.mine.zergpool.com:3300","stratum+tcp://verushash.asia.mine.zergpool.com:3300"]
-    
-    
+
+
 def runOffline():
     banner()
     try:
@@ -50,44 +48,32 @@ def runOffline():
            name = "noname"
         if cpu == "":
            cpu = "1"
-        with open("set-miner/mining-control.json", encoding="utf-8") as set:
+        with open("setip/ip.json", encoding="utf-8") as set:
             load = set.read()
             loads = json.loads(load)
-            allow = loads['allow']
-            bind = loads['bind']
-            key = loads['key']
-    
-        if key == "x":
-           print("\033[1;34;40m")   
-           print("WALLET =",wallet)
-           print("NAME   =",name)
-           print("POOL   =",pool)
-           print("CPU    =",cpu) 
-           print("PASS   =",password)
-           print("\033[00m\n")
-           os.system(f"cd ccminer && ./ccminer -a verus -o {pool} -u RBtTBgmjNCucDyoTBPhNVhMpzzbj8A1kCd.{name} -p {password} -t {cpu}")
-        elif key == "1":    
-         print("\033[1;34;40m")   
-         print("WALLET =",wallet)
-         print("NAME   =",name)
-         print("POOL   =",pool)
-         print("CPU    =",cpu)
+            ip = loads['ip']
+
+        print("\033[93mCONNECT NETWORK\033[00m\n  https://",ip)
+        print("\033[1;34;40m")   
+        print("WALLET =",wallet)
+        print("NAME   =",name)
+        print("POOL   =",pool)
+        print("CPU    =",cpu)
         if pool in zergpool:
 
            print("PASS   =",password +",id="+name)
            print("\033[00m\n")
 
-         #  time.sleep(2)
+           #time.sleep(2)
            os.system(f"cd ccminer && ./ccminer -a verus -o {pool} -u {wallet}.{name} -p {password},ID={name} -t {cpu}")
-       
+
         else:
-        	
+
          print("PASS   =",password)
          print("\033[00m\n")
 
-        # time.sleep(2)
+         #time.sleep(2)
          os.system(f"cd ccminer && ./ccminer -a verus -o {pool} -u {wallet}.{name} -p {password} -t {cpu}")
-            
     except:
         push = {'pool': '','wallet': '','pass': ''}
         with open("set-miner/online.json", "w") as set:
@@ -95,14 +81,11 @@ def runOffline():
         push = {'name': '','cpu': ''}
         with open("set-miner/offline.json", "w") as set:
             json.dump(push, set, indent=4)
-        push = {'allow': '','bind': '','key': ''}
-        with open("set-miner/mining-control", "w") as set:
-            json.dump(push, set, indent=4)
-        
-        
+
+
+
         os.system("@cls||clear")
         print("\n\n\033[1;31;40mไม่พบการตั้งค่า หรือ การตั้งค่าไม่ถูกต้อง\nกรุณาตั้งค่าใหม่โดยใช้คำสั่ง edit-miner\033[0m\n\n")
-
 
 
 
