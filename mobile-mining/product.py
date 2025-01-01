687b35ce217d5690a74e332074eff3ecb68d8373
saengx <157899625+saengx@users.noreply.github.com>
2025-01-01 08:49:09 +0700

    Update product.py

--- mobile-mining/product.py
+++ mobile-mining/product.py
@@ -1,5 +1,14 @@
-#!/bin/bash
+import os, json, requests
 
-cd
-cd ../etc/mobile-mining
-python3 addip.py
+
+def product():
+    os.system("clear")
+    print("\033[1;34;40m")
+    os.system("figlet -f ANSI_Shadow VERUS")
+    os.system("figlet -f digital server version")
+    #print("\033[00m\n")
+    print("\033[96mDeveloper by PICHET SAENGTAWAN\033[0m")
+    #print("\033[96m\033[0m")
+    #print("\033[36m\033[0m")
+
+    #print(f"\n\033[91mกำลัง Run Mining\033[0m\n")
