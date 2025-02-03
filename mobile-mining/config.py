d8d4905109e56a1b03c6ea0ebf3cb4940017b46d
saengx <157899625+saengx@users.noreply.github.com>
2025-02-04 02:54:30 +0700

    Update config.py

--- mobile-mining/config.py
+++ mobile-mining/config.py
@@ -3,9 +3,9 @@ import os, json, requests
 
 def banner():
     os.system("clear")
-    print("\033[1;34;40m")
+    print("\033[1;33;40m")
     os.system("figlet -f ANSI_Shadow VERUS")
-    os.system("figlet -f digital server version")
+    os.system("figlet -f TWO Miner version")
     #print("\033[00m\n")
     print("\033[96mDeveloper by PICHET SAENGTAWAN\033[0m")
     #print("\033[96m\033[0m")
