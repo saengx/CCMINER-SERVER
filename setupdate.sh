7b72b88a4686bf23a2455d8dbd5980d854d9e6b1
saengx <157899625+saengx@users.noreply.github.com>
2025-07-28 07:49:55 +0700

    Update setupdate.sh

--- setupdate.sh
+++ setupdate.sh
@@ -27,6 +27,10 @@ apt-get install wget -y
 apt-get install figlet -y
 apt-get install python3-progress -y
 apt-get install python3-requests -y
+pip install requests
+pip3 install py-cpuinfo
+pip3 install psutil
+apt-get install python3-psutil python3-cpuinfo
 
 
 apt-get install libllvm-16-ocaml-dev libllvm16 llvm-16 llvm-16-dev llvm-16-doc llvm-16-examples llvm-16-runtime clang-16 clang-tools-16 clang-16-doc libclang-common-16-dev libclang-16-dev libclang1-16 clang-format-16 python3-clang-16 clangd-16 clang-tidy-16 libclang-rt-16-dev libpolly-16-dev libfuzzer-16-dev lldb-16 lld-16 libc++-16-dev libc++abi-16-dev libomp-16-dev libclc-16-dev libunwind-16-dev libmlir-16-dev mlir-16-tools flang-16 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 libclang-rt-16-dev-wasm32 libclang-rt-16-dev-wasm64 -y
