7c2377a062c0b09d695a4b03ccc2f7f102fad582
saengx <157899625+saengx@users.noreply.github.com>
2025-10-14 03:41:26 +0700

    Update config.py

--- mobile-mining/config.py
+++ mobile-mining/config.py
@@ -25,4 +25,4 @@ def banneredit():
     print("\033[1;34;34m")
     os.system("figlet -f big [-EDIT-]")
     print("\033[00m\n")
-    print(f"\n\033[96mDeveloper by PICHET SAENGTEWAN\033[0m\n")
+    print(f"\n\033[96mDeveloper by PICHET SAENGTAWAN\033[0m\n")
