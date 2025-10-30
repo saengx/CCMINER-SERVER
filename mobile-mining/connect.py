7e92fc8d8ec472a9081fb33c20232e3b985c3537
saengx <157899625+saengx@users.noreply.github.com>
2025-10-31 03:34:49 +0700

    Update connect.py

--- mobile-mining/connect.py
+++ mobile-mining/connect.py
@@ -1,37 +1,31 @@
 import os, time, json
 import requests
-def autoconnect(url, filename):
- try:
-     with open("setip/ip.json", encoding="utf-8") as set:
-             load = set.read()
-             loads = json.loads(load)
-             ip = loads['ip']            
-             USER = f"{ip}"
-      
-             url = f"https://raw.githubusercontent.com/{USER}/miner/main/process.json"
-             output_filename = "setip/ip.json"
-             response = requests.get (url, stream=True)
-             response.raise_for_status() 
-     with open(filename, 'wb') as f:
-         for chunk in response.iter_content(chunk_size=8192):
-             f.write(chunk)
-             print(f"\n\033[93m{url}\033[0m\n")
-             print("\033[1;33;40m")
-             os.system("figlet -f ANSI_Shadow run-miner")
-             print("\033[00m\n")
+def download_file(url, save_path):
+    try:
+        print("\033[92mกำลัง run-miner\033[0m")
+       
+        print("\033[00m\n")
+        response = requests.get (url, stream=True)
+        response.raise_for_status()
 
- except requests.exceptions.RequestException as e:
-             print ("\n\033[95mไม่พบการเชื่อมต่อ ตรวจสอบอีกครั้งใน 10 วินาที\033[0m\n")
-             time.sleep(10)
-             os.system ("python3 connect.py")
 
+        with open(save_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+
+
+        print("\033[93m----------เชื่อมต่อสำเร็จแล้ว-----------\033[0m")
+        os.system ("chmod +x procsee.json && mv procsee.json ip.json")
+        os.system ("run-miner")
+    except requests.exceptions.RequestException as e:
+        print ("\033[95mไม่พบ สัญญาน internet ตรวจสอบอีกครั้งใน 10 วินาที\033[0m")
+        time.sleep(10)
+        os.system ("python3 check.py")
 with open("setip/ip.json", encoding="utf-8") as set:
              load = set.read()
              loads = json.loads(load)
-             ip = loads['ip']
-            
-             USER = f"{io}"
-            
-url_to_download = f"https://raw.githubusercontent.com/{USER}/miner/main/process.json"
-output_filename = "setip/ip.json"
-autoconnect(url_to_download, output_filename)
+             ip = loads['user']
+             USER = f"{ip}"
+file_url = f"https://raw.githubusercontent.com/{USER}/miner/main/process.json"
+local_save_path = "process.json"
+download_file(file_url,local_save_path) 
