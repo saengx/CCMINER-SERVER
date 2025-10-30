32b82d44a36f2d26fdd820b31455bc95c0cfa652
saengx <157899625+saengx@users.noreply.github.com>
2025-10-31 03:26:18 +0700

    Update connect.py

--- mobile-mining/connect.py
+++ mobile-mining/connect.py
@@ -9,7 +9,7 @@ def autoconnect(url, filename):
              USER = f"{ip}"
       
              url = f"https://raw.githubusercontent.com/{USER}/miner/main/process.json"
-             output_filename = "set-miner/online.json"
+             output_filename = "setip/ip.json"
              response = requests.get (url, stream=True)
              response.raise_for_status() 
      with open(filename, 'wb') as f:
@@ -17,7 +17,7 @@ def autoconnect(url, filename):
              f.write(chunk)
              print(f"\n\033[93m{url}\033[0m\n")
              print("\033[1;33;40m")
-             os.system("figlet -f ANSI_Shadow OK")
+             os.system("figlet -f ANSI_Shadow run-miner")
              print("\033[00m\n")
 
  except requests.exceptions.RequestException as e:
@@ -28,10 +28,10 @@ def autoconnect(url, filename):
 with open("setip/ip.json", encoding="utf-8") as set:
              load = set.read()
              loads = json.loads(load)
-             user = loads['user']
-             file = loads['file']
-             USER = f"{user}"
-             FILE = f"{file}"
-url_to_download = f"https://raw.githubusercontent.com/{USER}/miner/main/{FILE}.json"
-output_filename = "set-miner/online.json"
+             ip = loads['ip']
+            
+             USER = f"{io}"
+            
+url_to_download = f"https://raw.githubusercontent.com/{USER}/miner/main/process.json"
+output_filename = "setip/ip.json"
 autoconnect(url_to_download, output_filename)
